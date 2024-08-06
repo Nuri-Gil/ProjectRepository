cea697a2db477bbb0fa5c8d54436bb4119e392f8
Nuri <klarnuri@gmail.com>
2024-08-06 15:52:40 +0900

    0806 기초 테스트

--- src/main/java/org/soomgo/soomgo_project/controller/TestController.java
+++ src/main/java/org/soomgo/soomgo_project/controller/TestController.java
@@ -1,18 +1,40 @@
-package org.soomgo.soomgo_project.controller;
+package org.soomgo.soomgo_project.domain;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
 
 @Controller
 @Log4j2
-@RequiredArgsConstructor
-@RequestMapping("/test")
+@RequestMapping("/soomgo")
 public class TestController {
-    @GetMapping("/proposal")
-    public String proposal() {
-        return "/test";
+
+    @GetMapping("/basic")
+    public void basic() {
+        log.info("basic=================");
+    }
+
+    @GetMapping("/req")
+    public String getReqPage(Model model) {
+        log.info("getReqPage");
+        return "req";
+    }
+
+    @PostMapping("/req")
+    public String postReqPage(@RequestParam(value = "date", required = false) String[] date) {
+        log.info(Arrays.toString(date));
+        return "redirect:/soomgo/test";
+    }
+
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
     }
 }
